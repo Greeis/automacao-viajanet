9a9380afd34701a68efabd27c1b5e3365af15c47
Graziele Almeida <graaziele.182@gmail.com>
2019-05-19 21:13:12 -0300

    Criando o env

---
+++ features/support/env.rb
@@ -0,0 +1,9 @@
+require 'capybara'
+require 'capybara/cucumber'
+
+ Capybara.configure do |config|
+    config.default_driver = :selenium_chrome
+    #config.default_driver = :selenium
+    config.default_max_wait_time = 10
+    config.app_host = 'https://www.viajanet.com.br'
+end
